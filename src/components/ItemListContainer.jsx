500f2e041bea7cf76dd4f3bcc008b51be62920a6
Gustii27 <capuozzo.gustavo@gmail.com>
2023-06-07 16:47:43 -0300

    agregado filtrado por categoria

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,20 +1,31 @@
 import { useState, useEffect } from "react";
 import ItemList from "./ItemList";
 import { useParams } from "react-router-dom";
-import { collection, getDocs, getFirestore } from "firebase/firestore";
+import { collection, getDocs, getFirestore, query, where } from "firebase/firestore";
 
 const ItemListContainer = () => {
     const [items, setItems] = useState([]);
     const {id} = useParams();
     
     useEffect(() =>{
-        const db = getFirestore();
-        const collectionRef = collection(db, "items");
+        const fetchItems = async () => {
+            try {
+                const db = getFirestore();
+                const collectionRef = collection(db, "items");
+                
+                let queryRef = query(collectionRef);
+                if (id) {
+                    queryRef = query(collectionRef, where("categoria", "==", id));
+                }
 
-        getDocs(collectionRef).then((snapshot) =>{
-            setItems(()=> snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() })));
-        }).catch((error) => console.log({ error }))
-    }, []);
+                const querySnapshot = await getDocs(queryRef);
+                const fetchedItems = querySnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data()}));
+                setItems(fetchedItems);
+            } catch(error) {console.log({ error })};
+        }
+
+        fetchItems();        
+    }, [id]);
     
     return(
         <div>
