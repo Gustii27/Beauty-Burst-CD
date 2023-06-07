99fe00c136994c133398e1663f5159dae16774af
Gustii27 <capuozzo.gustavo@gmail.com>
2023-06-07 11:25:08 -0300

    ..

--- src/components/RootItem.jsx
+++ src/components/RootItem.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import NavBar from './NavBar'
 import ItemDetailContainer from './ItemDetailContainer'
-import "./rootItem.css";
+import "./rootItem.css"
 
 const RootItem = () => {
   return (
-    <div className='background-body'>
+    <>
         <NavBar/>
         <ItemDetailContainer />
-    </div>
+    </>
   )
 }
 
-export default RootItem
+export default RootItem;
