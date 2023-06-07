18ba965fd8cf8622282b152dd79d3a05a8f31be3
Gustii27 <capuozzo.gustavo@gmail.com>
2023-06-07 14:52:01 -0300

    instalando firebase

--- src/main.jsx
+++ src/main.jsx
@@ -10,6 +10,24 @@ import {
 import RootItem from './components/RootItem';
 import "./styles/global.css";
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDozCuoOJk3mEXECioFTkTos5Q2e-rcnxA",
+  authDomain: "beauty-burst.firebaseapp.com",
+  projectId: "beauty-burst",
+  storageBucket: "beauty-burst.appspot.com",
+  messagingSenderId: "585118359860",
+  appId: "1:585118359860:web:60168d2bc763de25d44561"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+
 const router = createBrowserRouter([
   {
     path: "/",
