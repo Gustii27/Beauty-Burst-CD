1896e85003b265cb3d42e74f6a1ec3897433750a
Gustii27 <capuozzo.gustavo@gmail.com>
2023-06-13 15:52:27 -0300

    creando y agregando .env

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import CartWidget from "./components/CartWidget";
 import Root from './components/Root';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {
@@ -17,12 +16,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDozCuoOJk3mEXECioFTkTos5Q2e-rcnxA",
-  authDomain: "beauty-burst.firebaseapp.com",
-  projectId: "beauty-burst",
-  storageBucket: "beauty-burst.appspot.com",
-  messagingSenderId: "585118359860",
-  appId: "1:585118359860:web:60168d2bc763de25d44561"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 // Initialize Firebase
@@ -43,7 +42,7 @@ const router = createBrowserRouter([
   },
   {
     path: "/cart",
-    element: <CartWidget/>,
+    element: <Cart/>,
   },
 ]);
 
